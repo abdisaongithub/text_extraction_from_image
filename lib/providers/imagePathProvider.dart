bd6b0838aa906ef688ea9e94a6acc73b70ff585f
abdi <abdtsegaye@gmail.com>
2022-05-21 12:34:58 +0300

    updated all the plugins, added vibration after reading the text

--- lib/providers/imagePathProvider.dart
+++ lib/providers/imagePathProvider.dart
@@ -2,6 +2,7 @@ import 'dart:io' show File;
 
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_vibrate/flutter_vibrate.dart';
 import 'package:fluttertoast/fluttertoast.dart';
 import 'package:google_mlkit_text_recognition/google_mlkit_text_recognition.dart';
 
@@ -22,6 +23,11 @@ class ImagePathNotifier extends StateNotifier<String> {
     final recognized = await textRecognizer.processImage(inputImage);
 
     state = state + recognized.text;
+
+    if (await Vibrate.canVibrate) {
+      Vibrate.feedback(FeedbackType.light);
+    }
+
     Fluttertoast.showToast(
       msg: "Image Scanned, Move on to the next Page",
       toastLength: Toast.LENGTH_SHORT,
